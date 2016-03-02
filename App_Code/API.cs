5bc5daa812a5cb28a60521a5c8816ae6a2285d78
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-03-02 11:32:12 -0600

    initial

---
+++ App_Code/API.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+[System.Web.Script.Services.ScriptService]
+public class API : System.Web.Services.WebService
+{
+
+    public API()
+    {
+
+        //Uncomment the following line if using designed components
+        //InitializeComponent();
+    }
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+    public void GetFullTreeByPartNumber(string PartNumber)
+    {
+
+        Context.Response.Clear();
+        Context.Response.ContentType = "application/json";
+
+        Dictionary<string, Item> obj = GetFullTreeFromDb(PartNumber);
+
+
+        var jss = new JavaScriptSerializer();
+        string output = jss.Serialize(obj);
+        //string output = JsonConvert.SerializeObject(obj);
+
+        Context.Response.Write(output);
+
+    }
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+    public void GetFullTreeByPartNumberJSONP(string PartNumber, string Callback)
+    {
+
+        Context.Response.Clear();
+        Context.Response.ContentType = "application/javascript";
+
+        Dictionary<string, Item> obj = GetFullTreeFromDb(PartNumber);
+
+
+        var jss = new JavaScriptSerializer();
+        string output = jss.Serialize(obj);
+        output = Callback + "(" + output + ");";
+
+        Context.Response.Write(output);
+
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+    public void GetFullTreeByCagePartSerial(string CageCode, string PartNumber, string SerialNumber)
+    {
+        Context.Response.Clear();
+        Context.Response.ContentType = "application/json";
+
+        Dictionary<string, Item> obj = GetFullTreeFromDb(CageCode, PartNumber, SerialNumber);
+
+        var jss = new JavaScriptSerializer();
+        string output = jss.Serialize(obj);
+
+        Context.Response.Write(output);
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+    public void GetFullTreeByCagePartSerialJSONP(string CageCode, string PartNumber, string SerialNumber, string Callback)
+    {
+        Context.Response.Clear();
+        Context.Response.ContentType = "application/javascript";
+
+        Dictionary<string, Item> obj = GetFullTreeFromDb(CageCode, PartNumber, SerialNumber);
+
+        var jss = new JavaScriptSerializer();
+        string output = jss.Serialize(obj);
+        output = Callback + "(" + output + ");";
+
+        Context.Response.Write(output);
+    }
+
+    public List<Item> GetFlatFromDb()
+    {
+
+        IMSDataContext db = new IMSDataContext();
+        IEnumerable<ITEMTREE> results = db.ExecuteQuery<ITEMTREE>
+            (@"	WITH myTree (Id, ParentId, LocPart)
+	            AS (
+	            -- anchor member declaration
+	            SELECT ID, ID_PARENT, LOCATION_PART FROM ITEMTREE WHERE PART_NUMBER='some-part-number'
+	            UNION ALL
+	            -- recursive member declaration
+	            SELECT ITEMTREE.ID, ITEMTREE.ID_PARENT, ITEMTREE.LOCATION_PART FROM ITEMTREE
+	            INNER JOIN myTree ON myTree.ParentId = ITEMTREE.ID
+	            )
+	            SELECT DISTINCT * FROM ITEMTREE INNER JOIN myTree ON myTree.Id = ITEMTREE.ID"
+            );
+
+        List<Item> items = new List<Item>();
+
+        foreach (ITEMTREE itemtree in results)
+        {
+            items.Add(new Item(itemtree));
+        }
+
+
+        return items;
+    }
+
+
+    public Dictionary<string, Item> GetFullTreeFromDb(string PartNumber)
+    {
+
+        IMSDataContext db = new IMSDataContext();
+
+        IEnumerable<ITEMTREE> results = db.ExecuteQuery<ITEMTREE>
+        (@"	WITH ancestors (Id, ParentId)
+            AS (
+            -- anchor member declaration
+            SELECT ID, ID_PARENT FROM ITEMTREE WHERE PART_NUMBER = {0}
+
+            UNION ALL
+
+            -- recursive member declaration
+            SELECT ITEMTREE.ID, ITEMTREE.ID_PARENT FROM ITEMTREE
+            INNER JOIN ancestors ON ancestors.ParentId = ITEMTREE.ID
+            ),
+
+            descendants (Id, ParentId)
+            AS (
+            -- anchor member declaration
+            SELECT ID, ID_PARENT FROM ITEMTREE WHERE PART_NUMBER = {1}
+
+            UNION ALL
+
+            -- recursive member declaration
+            SELECT ITEMTREE.ID, ITEMTREE.ID_PARENT FROM ITEMTREE
+            INNER JOIN descendants ON descendants.Id = ITEMTREE.ID_PARENT
+            )
+
+            SELECT DISTINCT * FROM ITEMTREE RIGHT JOIN ancestors ON ancestors.Id = ITEMTREE.ID
+
+            UNION
+
+            SELECT DISTINCT * FROM ITEMTREE RIGHT JOIN descendants ON descendants.Id = ITEMTREE.ID", PartNumber, PartNumber
+        );
+
+
+        Dictionary<string, Item> items = new Dictionary<string, Item>();
+
+        List<string> IDs = new List<string>();
+
+        foreach (ITEMTREE itemtree in results)
+        {
+            items.Add(itemtree.ID.ToString(), new Item(itemtree));
+            IDs.Add(itemtree.ID.ToString());
+        }
+
+        int id_parent;
+
+        foreach (string id in IDs)
+        {
+
+            if (items[id].ID_PARENT != null)
+            {
+                id_parent = items[id].ID_PARENT.GetValueOrDefault();
+                items[id_parent.ToString()].addChild(items[id]);
+
+            }
+
+        }
+
+        // clear out non-root items (non-root items now contained
+        // within parent-child structure below root items)
+        foreach (string id in IDs)
+        {
+            if (items[id].ID_PARENT != null)
+            {
+                items.Remove(id);
+            }
+        }
+
+        return items;
+    }
+
+
+
+
+    public Dictionary<string, Item> GetFullTreeFromDb(string CageCode, string PartNumber, string SerialNumber)
+    {
+
+        IMSDataContext db = new IMSDataContext();
+
+        IEnumerable<ITEMTREE> results = db.ExecuteQuery<ITEMTREE>
+        (@"	WITH ancestors (Id, ParentId)
+            AS (
+            -- anchor member declaration
+            SELECT ID, ID_PARENT FROM ITEMTREE WHERE CAGE = {0} AND PART_NUMBER = {1} AND S_N = {2}
+
+            UNION ALL
+
+            -- recursive member declaration
+            SELECT ITEMTREE.ID, ITEMTREE.ID_PARENT FROM ITEMTREE
+            INNER JOIN ancestors ON ancestors.ParentId = ITEMTREE.ID
+            ),
+
+            descendants (Id, ParentId)
+            AS (
+            -- anchor member declaration
+            SELECT ID, ID_PARENT FROM ITEMTREE WHERE CAGE = {0} AND PART_NUMBER = {1} AND S_N = {2}
+
+            UNION ALL
+
+            -- recursive member declaration
+            SELECT ITEMTREE.ID, ITEMTREE.ID_PARENT FROM ITEMTREE
+            INNER JOIN descendants ON descendants.Id = ITEMTREE.ID_PARENT
+            )
+
+            SELECT DISTINCT * FROM ITEMTREE RIGHT JOIN ancestors ON ancestors.Id = ITEMTREE.ID
+
+            UNION
+
+            SELECT DISTINCT * FROM ITEMTREE RIGHT JOIN descendants ON descendants.Id = ITEMTREE.ID", CageCode, PartNumber, SerialNumber
+        );
+
+
+        Dictionary<string, Item> items = new Dictionary<string, Item>();
+
+        List<string> IDs = new List<string>();
+
+        foreach (ITEMTREE itemtree in results)
+        {
+            items.Add(itemtree.ID.ToString(), new Item(itemtree));
+            IDs.Add(itemtree.ID.ToString());
+        }
+
+        int id_parent;
+
+        foreach (string id in IDs)
+        {
+
+            if (items[id].ID_PARENT != null)
+            {
+                id_parent = items[id].ID_PARENT.GetValueOrDefault();
+                items[id_parent.ToString()].addChild(items[id]);
+
+            }
+
+        }
+
+        // clear out non-root items (non-root items now contained
+        // within parent-child structure below root items)
+        foreach (string id in IDs)
+        {
+            if (items[id].ID_PARENT != null)
+            {
+                items.Remove(id);
+            }
+        }
+
+        return items;
+    }
+
+}
---
+++ App_Code/Item.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Summary description for Item
+/// </summary>
+public class Item
+{
+
+    public int ID { get; private set; }
+
+    public System.Nullable<int> ID_PARENT { get; private set; }
+
+    public string PART_NUMBER { get; private set; }
+
+    public string S_N { get; private set; }
+
+    public string CAGE { get; private set; }
+
+    public string ACRONYM { get; private set; }
+
+    public string R_OPS { get; private set; }
+
+    public string E_OPS { get; private set; }
+
+    public string BARCODE { get; private set; }
+
+    public string LABEL { get; private set; }
+
+    public string LOCATION_CODE { get; private set; }
+
+    public System.Nullable<decimal> QPU { get; private set; }
+
+    public System.Nullable<decimal> WIDTH { get; private set; }
+
+    public System.Nullable<decimal> HEIGHT { get; private set; }
+
+    public System.Nullable<decimal> LENGTH { get; private set; }
+
+    public System.Nullable<decimal> VOLUME { get; private set; }
+
+    public System.Nullable<decimal> DIAMETER { get; private set; }
+
+    public System.Nullable<decimal> WEIGHT { get; private set; }
+
+    //public System.Nullable<System.DateTime> EXPIRE_DATE { get; private set; }
+
+    //public string ITEM_SCHEMA { get; private set; }
+
+    //public string NOMER { get; private set; }
+
+    public string LAUNCH { get; private set; }
+
+    public string NOTES { get; private set; }
+
+    //public string TYPE { get; private set; }
+
+    //public string HAZARD { get; private set; }
+
+    //public string STATE { get; private set; }
+
+    //public string STATUS { get; private set; }
+
+    //public bool CONTAINER { get; private set; }
+
+    //public bool MOVEABLE { get; private set; }
+
+    //public string BACKGROUND { get; private set; }
+
+    //public string ICON { get; private set; }
+
+    //public bool SHOW_ICON { get; private set; }
+
+    //public System.Nullable<int> IMAGE_X { get; private set; }
+
+    //public System.Nullable<int> IMAGE_Y { get; private set; }
+
+    //public System.Nullable<int> IMAGE_W { get; private set; }
+
+    //public System.Nullable<int> IMAGE_H { get; private set; }
+
+    //public string SYSTEM { get; private set; }
+
+    //public string SUBSYSTEM { get; private set; }
+
+    //public string HOMELOCATION { get; private set; }
+
+    //public string INUSENAME { get; private set; }
+
+    //public System.Nullable<System.DateTime> ACT_DATE { get; private set; }
+
+    //public System.Nullable<System.DateTime> MOVE_DATE { get; private set; }
+
+    //public string USERNAME { get; private set; }
+
+    public string LOCATION_PART { get; private set; }
+
+    //public System.Nullable<System.DateTime> AUDIT_DATE { get; private set; }
+
+    //public System.Nullable<int> AUDIT_STATUS { get; private set; }
+
+    //public string AUDIT_LOCATION { get; private set; }
+
+    //public string FILL_STATUS { get; private set; }
+
+    //public System.Nullable<decimal> EMPTY_SPACE { get; private set; }
+
+    //public string DISPOSE { get; private set; }
+
+    //public string AUDIT_DBNAME { get; private set; }
+
+    //public System.Nullable<int> AUDIT_ID { get; private set; }
+
+    //public string AUDIT_COMMENT { get; private set; }
+
+    //public System.Nullable<bool> RFID_TAG_PRESENT { get; private set; }
+
+    public Dictionary<string, Item> CHILDREN { get; private set; }
+
+
+    public Item()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public Item(ITEMTREE itemtree)
+    {
+        ID = itemtree.ID;
+
+        ID_PARENT = itemtree.ID_PARENT;
+
+        PART_NUMBER = itemtree.PART_NUMBER;
+
+        S_N = itemtree.S_N;
+
+        CAGE = itemtree.CAGE;
+
+        ACRONYM = itemtree.ACRONYM;
+
+        R_OPS = itemtree.R_OPS;
+
+        E_OPS = itemtree.E_OPS;
+
+        BARCODE = itemtree.BARCODE;
+
+        LABEL = itemtree.LABEL;
+
+        LOCATION_CODE = itemtree.LOCATION_CODE;
+
+        NOTES = itemtree.NOTES;
+
+        LOCATION_PART = itemtree.LOCATION_PART;
+
+        QPU = itemtree.QPU;
+
+        WIDTH = itemtree.WIDTH;
+
+        HEIGHT = itemtree.HEIGHT;
+
+        LENGTH = itemtree.LENGTH;
+
+        VOLUME = itemtree.VOLUME;
+
+        DIAMETER = itemtree.DIAMETER;
+
+        WEIGHT = itemtree.WEIGHT;
+
+        LAUNCH = itemtree.LAUNCH;
+
+        CHILDREN = new Dictionary<string, Item>();
+    }
+
+    public void addChild(Item child)
+    {
+        CHILDREN.Add(child.ID.ToString(), child);
+    }
+
+}
